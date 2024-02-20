73280c1f448d190a40107a3c15c828c02911f218
Dev Mytho <118110516+DevMytho@users.noreply.github.com>
2024-02-21 04:36:10 +0530

    .

--- app/octabeat/lib/main.dart
+++ app/octabeat/lib/main.dart
@@ -28,10 +28,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 3, 3, 3)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Octabeat'),
     );
   }
 }
